af374f75670830996ac65ea67ad0d0c5277f0750
GreatKorn <elisey.ishim@yandex.ru>
2023-12-28 19:05:23 +0500

    start

--- temp_php/header.php
+++ temp_php/header.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="/src/css/slick.css"/>
     <link rel="stylesheet" type="text/css" href="/src/css/slick-theme.css"/>
-    <link rel="stylesheet" href="./src/css/bootstrap-grid.min.css">
-    <link rel="stylesheet" href="./src/css/style.css">
+    <link rel="stylesheet" href="/src/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="/src/css/style.css">
     <title>Pie</title>
 </head>
 <body>
